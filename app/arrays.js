f0e72e7b36b12bd5bf93ad2e3e3d7ea1d958f308
MKS-Student <mks.student@hackreactor.com>
2015-11-30 11:12:45 -0800

    Completed up to arrays/removeWithoutCopy

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,57 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
+    var found = -1;
 
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        found = i;
+      }
+    }
+
+    return found;
   },
 
   sum : function(arr) {
+    var temp = 0;
 
+    for (var i = 0; i < arr.length; i++) {
+      temp += arr[i];
+    }
+
+    return temp;
   },
 
   remove : function(arr, item) {
+    var newArr = [];
 
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] != item) {
+        newArr.push(arr[i]);
+      }
+    }
+
+    return newArr;
   },
 
   removeWithoutCopy : function(arr, item) {
+    var count = 0;
 
+    var del = function(found,array) {
+      for (var i = 0; i < array.length; i++) {
+        if (found === array[i]) {
+          array.splice(i,1);
+        }
+      }
+    }
+    console.log("orig:",arr);
+    for (var i = 0; i < arr.length; i++) {
+      del(item,arr);
+    }
+
+    // delete(item,arr);
+    console.log("final:",arr);
+    return arr;
   },
 
   append : function(arr, item) {
